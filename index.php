10091f9f15a1d13bdc2c881b26db09e502beb2c1
eduardonaranjo <eduardonaranjo@en-macbook-air.home>
2018-11-13 22:12:25 -0500

    added dynamic image

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>CIS385</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <h3>CIS385 Web Programming I</h3>
+        <ul>
+            <li><a>Resume</a></li>
+            <li><a>Resume</a></li>
+            <li><a>Resume</a></li>
+            <li><a href="arrays/">Arrays</a></li>
+        </ul>
+    </body>
+</html>
